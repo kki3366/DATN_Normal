5d0da57c1ac52f25ccc39234630be24e3d14e274
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-09-11 20:08:02 +0700

    root project

---
+++ Normal_DATN/src/main/java/com/DATN/NormalDatnApplication.java
@@ -0,0 +1,13 @@
+package com.DATN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalDatnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormalDatnApplication.class, args);
+	}
+
+}
