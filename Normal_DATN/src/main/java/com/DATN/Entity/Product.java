00d5e53e0e6ddd82cd850826b9fceaf3c975983d
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-09-11 20:45:41 +0700

    update x

--- Normal_DATN/src/main/java/com/DATN/Entity/Product.java
+++ Normal_DATN/src/main/java/com/DATN/Entity/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -65,5 +66,7 @@ public class Product {
 	boolean special;
 	
 	
-	
+	@ManyToOne
+	@JoinColumn(name = "CategoryId")
+	Category category;
 }
