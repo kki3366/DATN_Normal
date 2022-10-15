1953ce24a871570e38b55abbb927865d075e44fa
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-10-15 18:27:30 +0700

    upload bower templates

--- Normal_DATN/src/main/java/com/DATN/Controller/Admin/AdminDashboardController.java
+++ Normal_DATN/src/main/java/com/DATN/Controller/Admin/AdminDashboardController.java
@@ -1,5 +1,15 @@
 package com.DATN.Controller.Admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminDashboardController {
 
+	
+	@GetMapping("test")
+	public String t(){
+		return "Admin/components/header";
+	}
+	
 }
