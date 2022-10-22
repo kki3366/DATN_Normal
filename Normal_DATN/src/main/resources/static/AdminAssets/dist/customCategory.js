98a59886d818d8e70fe27cf910482d21e8b3bce9
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-10-22 20:52:48 +0700

    tiếp tục bí cmn. Chết tiệt

--- Normal_DATN/src/main/resources/static/AdminAssets/dist/customCategory.js
+++ Normal_DATN/src/main/resources/static/AdminAssets/dist/customCategory.js
@@ -8,37 +8,45 @@ if (convertUrl.pathname = '/admin/category') {
 	$(document).ready(function() {
 		var getCategoryUrl = protocol + '//' + hostname + ':' + port + '/api/categories'
 		//get All
-		//setInterval(function(){
-			
-		//})
-		
-		/*$.ajax({
-			method: 'get',
-			dataType: 'json',
-			url: getCategoryUrl,
-			success: function(datas) {
-				var result = ""
-				datas.forEach(item => {
-					result += `
+		var result = ""
+		function getDataCategory() {
+			$.ajax({
+				method: 'get',
+				dataType: 'json',
+				url: getCategoryUrl,
+				success: function(datas) {
+					datas.forEach(item => {
+						result += `
 						<tr role="row" class="even">
-                  			<td class="sorting_1">${item.id}</td>
-                  			<td>${item.nameCategory}</td>
-                		</tr>
+								<td class="sorting_1">${item.id}</td>
+								<td>${item.nameCategory}</td>
+						</tr>
 					`;
-				});
-				$("#tableCategory").append(result)
-			}
-		})	*/
-		
-		// post
-		/*var categoryForm = {
-			nameCategory: document.getElementById('nameCategory').value 
+						$("#tableCategory").append(result)
+					});
+				}
+			})
 		}
-		
-		$("#submitCategory").click(function(){
-     	 console.log(categoryForm);
-    });*/
-		
+		getDataCategory()
+
+
+		$('#submitCategory').click(function() {
+			var categoryForm = {
+				nameCategory: $('#nameCategory').val()
+			}
+			$.ajax({
+				method: 'POST',
+				dataType: 'json',
+				url: getCategoryUrl,
+				contentType: "application/json; charset=utf-8",
+				data: JSON.stringify(categoryForm),
+				success: function(datas) {
+					
+				}
+			})
+			
+		});
+
 	});
 } else {
 	console.log("not ok")
