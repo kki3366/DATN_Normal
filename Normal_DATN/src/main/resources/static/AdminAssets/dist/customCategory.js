bb63cdd937f80d7a59c6405a6e2197487a93ec6c
kki3366 <tinhyeudachet_lamlaitudau@yahoo.com.vn>
2022-10-27 20:42:01 +0700

    upload entry, paging, search

--- Normal_DATN/src/main/resources/static/AdminAssets/dist/customCategory.js
+++ Normal_DATN/src/main/resources/static/AdminAssets/dist/customCategory.js
@@ -4,35 +4,29 @@ var protocol = window.location.protocol
 baseUrl = protocol + '//' + hostname + ':' + port
 currentUrl = window.location.href;
 var convertUrl = new URL(currentUrl)
+
 if (convertUrl.pathname = '/admin/category') {
 	$(document).ready(function() {
 		var getCategoryUrl = protocol + '//' + hostname + ':' + port + '/api/categories'
-
-		var x = $.ajax({
-			method: 'get',
+		// Call data from API
+		$.ajax({
+			method: 'GET',
 			dataType: 'json',
 			async: false,
 			url: getCategoryUrl,
-			success: function(resp) {
-
+			success: function(data) {
+				$('#tableCategory').DataTable({
+					data: data,
+					pageLength: 5,
+					lengthMenu: [5, 10, 20,25,50],
+					columns: [
+						{ 'data': 'id' },
+						{ 'data': 'nameCategory' }
+					]
+				});
 			}
-		}).responseJSON;
-
-		console.log(JSON.stringify(x))
-		$('#tableCategory1').DataTable({
-			processing: true,
-			serverSide: true,
-			ajax: {
-				url: getCategoryUrl,
-				dataSrc: ''
-			},
-			columns: [
-				{ data: 'id' },
-				{ data: 'nameCategory' }	
-			]
 		});
-		
-		
+
 		$('#submitCategory').click(function() {
 			var categoryForm = {
 				nameCategory: $('#nameCategory').val()
@@ -44,14 +38,16 @@ if (convertUrl.pathname = '/admin/category') {
 				contentType: "application/json; charset=utf-8",
 				data: JSON.stringify(categoryForm),
 				success: function(datas) {
-					$("#tableCategory1").DataTable().ajax.reload();
+					
+					$("#tableCategory").DataTable().ajax.reload();
 				},
-				error: function(resp){
+				error: function(resp) {
 					console.log(resp.responseText)
 				}
 			})
 		});
-
+		
+		
 	});
 } else {
 	console.log("not ok")
