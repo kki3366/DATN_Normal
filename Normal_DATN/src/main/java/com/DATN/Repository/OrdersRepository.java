445ad93f641d0cb45230a90e33fa3c6fa6302c21
HoLy <HoLy@DESKTOP-C2KB8R8>
2022-11-05 19:34:09 +0700

    lich su dat hang

--- Normal_DATN/src/main/java/com/DATN/Repository/OrdersRepository.java
+++ Normal_DATN/src/main/java/com/DATN/Repository/OrdersRepository.java
@@ -1,9 +1,14 @@
 package com.DATN.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import com.DATN.Entity.Orders;
 
 public interface OrdersRepository extends JpaRepository<Orders, Integer>{
-
+	@Query("SELECT o FROM Orders o WHERE o.user.id=?1")
+    List<Orders>  findByIdUser(String user);
 }
