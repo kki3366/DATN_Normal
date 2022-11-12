256b792f2b3d127c8cda7f58756e54477b387d0c
HoLy <HoLy@DESKTOP-C2KB8R8>
2022-11-12 21:19:47 +0700

    ,,,

--- Normal_DATN/src/main/java/com/DATN/Controller/NguoiDung/index.java
+++ Normal_DATN/src/main/java/com/DATN/Controller/NguoiDung/index.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.DATN.Entity.Category;
 import com.DATN.Entity.Product;
+import com.DATN.Repository.CategoryRepository;
 import com.DATN.Repository.ProductRepository;
 
 
@@ -16,11 +18,15 @@ import com.DATN.Repository.ProductRepository;
 public class index {
 	@Autowired
 	ProductRepository productRepository;
+	@Autowired
+	CategoryRepository categorytRepository;
 
 	@RequestMapping("/index")
 	public String form(Model model) {
 		List<Product> item = productRepository.findAll();
+		List<Category> category = categorytRepository.findAll();
 		model.addAttribute("item", item);
+		model.addAttribute("category", category);
 		return "nguoiDung/index";
 	}
 }
