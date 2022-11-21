1474123c5dac89f68a1270a496f25105fc21ad0c
HoLy <HoLy@DESKTOP-C2KB8R8>
2022-11-21 23:46:34 +0700

    pull về ko có j hết buồn vl

--- Normal_DATN/src/main/java/com/DATN/Controller/Admin/OrderController.java
+++ Normal_DATN/src/main/java/com/DATN/Controller/Admin/OrderController.java
@@ -13,13 +13,16 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.DATN.SessionService;
+import com.DATN.Entity.OrderDetail;
 import com.DATN.Entity.Orders;
 import com.DATN.Entity.users;
+import com.DATN.Repository.OrderDetailRepository;
 import com.DATN.Repository.OrdersRepository;
 import com.DATN.Service.OrderService;
 import com.DATN.Service.OrderServiceImpl;
@@ -35,11 +38,15 @@ public class OrderController {
 	@Autowired
 	OrderServiceImpl orderServiceImpl;
 	@Autowired
+	OrderDetailRepository orderDetailRepository;
+	@Autowired
 	SessionService session;	
 	@RequestMapping("/admin/order")
-	public String form(Model m) {
-		List<Orders> orderList = orderServiceImpl.findAll();
-		m.addAttribute("orderList",orderList);
+	public String form(Model m,	@RequestParam("p") Optional<Integer> p) {
+		Pageable pageable = PageRequest.of(p.orElse(0), 5);
+		Page<Orders> page = ordersRepository.findAll(pageable);
+		m.addAttribute("page",page);
+		m.addAttribute("edit",false);
 
 		m.addAttribute("order",new Orders());
 		return "Admin/page/order";
@@ -53,12 +60,14 @@ public class OrderController {
 		}
 	
 	@RequestMapping("/admin/order/edit")
-	public String edit(Model m,@RequestParam("edit") Boolean edit,@RequestParam("order") Integer Order) {
+	public String edit(Model m,@RequestParam("edit") Boolean edit,@RequestParam("order") Integer Order
+			,	@RequestParam("p") Optional<Integer> p) {
 		Orders ord = ordersRepository.getById(Order);
 		m.addAttribute("order",ord);
 		m.addAttribute("edit",edit);
-		List<Orders> orderList = orderServiceImpl.findAll();
-		m.addAttribute("orderList",orderList);
+		Pageable pageable = PageRequest.of(p.orElse(0), 5);
+		Page<Orders> page = ordersRepository.findAll(pageable);
+		m.addAttribute("page",page);
 		return "Admin/page/order";
 	}
 	@RequestMapping("/admin/order/find")
@@ -72,7 +81,15 @@ public class OrderController {
 		Pageable pageable = PageRequest.of(p.orElse(0), 5);
 		Page<Orders> page = ordersRepository.findByKeywords("%"+kwords+"%", pageable);
 		model.addAttribute("page", page);
-		return "redirect:/admin/order";
+		model.addAttribute("order",new Orders());
+		model.addAttribute("keywords", kwords);
+		return "Admin/page/order";
+	}
+	@RequestMapping("/admin/orderDetail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		List<OrderDetail> item = orderDetailRepository.findByIdOrder(id);
+		model.addAttribute("item", item);
+		return "Admin/page/orderDetail";
 	}
 }
 	
