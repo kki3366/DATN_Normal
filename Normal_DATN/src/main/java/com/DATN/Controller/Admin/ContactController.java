2a12e5a36dacf85ba63e2dd00bcb4962f98c9262
Hp <Hp@DESKTOP-RSVF4S4>
2022-11-25 15:09:43 +0700

    :((((

--- Normal_DATN/src/main/java/com/DATN/Controller/Admin/ContactController.java
+++ Normal_DATN/src/main/java/com/DATN/Controller/Admin/ContactController.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,6 +29,8 @@ public class ContactController {
 		Page<Contact> pageList = contact.findAll(page);
 		m.addAttribute("page",pageList);
 		m.addAttribute("contact",new Contact());
+		m.addAttribute("edit",false);
+		m.addAttribute("send",false);
 		return "Admin/page/ContactAd";
 	}
 	
@@ -38,6 +42,39 @@ public class ContactController {
 		Pageable page = PageRequest.of(p.orElse(0), 5);
 		Page<Contact> pageList = contact.findAll(page);
 		m.addAttribute("page",pageList);
+		m.addAttribute("edit",false);
+		m.addAttribute("send",true);
+		return "Admin/page/ContactAd";
+	}
+	@RequestMapping("/admin/contact/writefeedback")
+	public String phanhoi(Model m,@RequestParam("p") Optional<Integer> p) {
+		
+		m.addAttribute("contact",new Contact());
+		Pageable page = PageRequest.of(p.orElse(0), 5);
+		Page<Contact> pageList = contact.findAll(page);
+		m.addAttribute("page",pageList);
+
+		return "Admin/page/ContactAd";
+	}
+	@RequestMapping("/admin/contact/feedback")
+	public String phanhoi(Model m,@RequestParam("subject") String subject,@RequestParam("content") String content,@RequestParam("p") Optional<Integer> p) {
+		Boolean check = true;
+		if(subject.isEmpty()) {
+			m.addAttribute("subject","Bạn chưa nhập tiêu đề");
+			check = false;
+		}
+		if(content.isEmpty()) {
+			m.addAttribute("content","Bạn chưa nhập nội dung");
+			check = false;
+		}
+		if(check == true) {
+			
+		}
+		m.addAttribute("contact",new Contact());
+		Pageable page = PageRequest.of(p.orElse(0), 5);
+		Page<Contact> pageList = contact.findAll(page);
+		m.addAttribute("page",pageList);
+
 		return "Admin/page/ContactAd";
 	}
 }
