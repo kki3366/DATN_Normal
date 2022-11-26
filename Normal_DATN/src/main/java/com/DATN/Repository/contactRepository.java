053d0c1da91414176b7df75c4386a4dbe1a3937a
Hp <Hp@DESKTOP-RSVF4S4>
2022-11-26 14:26:10 +0700

    Xong contact

--- Normal_DATN/src/main/java/com/DATN/Repository/contactRepository.java
+++ Normal_DATN/src/main/java/com/DATN/Repository/contactRepository.java
@@ -1,9 +1,15 @@
 package com.DATN.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.DATN.Entity.Contact;
 
+
 public interface contactRepository extends JpaRepository<Contact,Integer> {
 
+	@Query("SELECT o FROM Contact o WHERE o.name LIKE ?1")
+	Page<Contact> findByKeywords(String keywords, Pageable pgeable);
 }
