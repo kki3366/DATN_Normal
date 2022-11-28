4e92fd68282fd99631155f5f09525057595d1594
HoLy <HoLy@DESKTOP-C2KB8R8>
2022-11-28 18:19:24 +0700

    ...

--- Normal_DATN/src/main/java/com/DATN/Entity/Contact.java
+++ Normal_DATN/src/main/java/com/DATN/Entity/Contact.java
@@ -15,10 +15,11 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+@Data
 @Entity
 @Table(name = "Contact")
 @Getter
@@ -58,6 +59,70 @@ public class Contact {
 	@Column(name = "Contents")
 	@NotBlank(message = "Bạn chưa nhập nội dung")
 	String Contents;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public users getUser() {
+		return user;
+	}
+
+	public void setUser(users user) {
+		this.user = user;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContents() {
+		return Contents;
+	}
+
+	public void setContents(String contents) {
+		Contents = contents;
+	}
 
 	
 }
